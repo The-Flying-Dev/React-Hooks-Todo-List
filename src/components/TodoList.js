2fd22e0e8a36326e83d75847b81fc880f99ef957
work <leandranm@outlook.com>
2022-06-27 21:38:48 -0700

    useEffect Example

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddNewTodo from './AddNewTodo';
 
 const TodoList = () => {
 
+  //create useState
   const [todos, setTodos] = useState([
     { text: 'Work', id: 1 },
     { text: 'Practice', id: 2 },
     { text: 'Debug', id: 3 }
   ]);
 
+  const [count, setCount] = useState(0);
+
+  //deconstruct then set
   const addTodo = (text) => {
     setTodos([...todos,
       { text, id: Math.random() }
     ]);
   };
+
+  useEffect(() => {
+    console.log('use effect', todos);
+  }, [todos]);
+
+  useEffect(() => {
+    console.log('use effect', count);
+  }, [count]);
 
   return (
     <div>
@@ -25,6 +37,7 @@ const TodoList = () => {
         })}
       </ul>
       <AddNewTodo addTodo={addTodo} />
+      <button onClick={() => setCount(count + 1)}>Score: {count}</button>
     </div>
   )
 };
